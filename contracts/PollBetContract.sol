249705d385082f63075df58a1c0a80f48ec56d1a
apurvaah <apurvaah@my.utexas.edu>
2023-04-02 18:47:29 -0500

    added pollcreation

--- contracts/PollBetContract.sol
+++ contracts/PollBetContract.sol
@@ -7,17 +7,52 @@ contract PollBetContract {
         address creator;
         uint startTime;
         uint endTime;
-        //mapping(address => uint) votes;
+        string[] options;
+        mapping(address => uint) votes;
+        bool isOpen;
     }
 
     struct Bet {
         uint pollId;
-        uint pollChoice;
+        string pollChoice;
         address user;
         uint amount;
     }
 
-    function createPoll(){}
+    // Events
+    event PollCreated(uint indexed id, address indexed creator, uint startTime, uint endTime, string[] options);
+    event PollClosed(uint indexed id, address indexed winner);
+    event BetPlaced(uint indexed pollId, string pollChoice, address indexed user, uint amount);
+    event RewardsDistributed(uint indexed pollId, address[] winners, uint[] rewards);
+
+    // Variables
+    uint public pollCount;
+    uint public betCount;
+    mapping(uint => Poll) public polls;
+    mapping(uint => uint) public pollBetTotal;
+    mapping(uint => mapping(address => uint)) public pollBets;
+    mapping(uint => uint[]) public pollWinners;
+    mapping(address => uint) public balances;
+    mapping(uint => Bet[]) public pollBetsList;
+
+    function createPoll(uint _startTime, uint _endTime, string[] memory _options) public {
+        require(_endTime > _startTime, "Invalid poll end time");
+        require(_options.length > 1, "At least two options required");
+
+        pollCount++;
+        polls[pollCount] = Poll({
+            id: pollCount,
+            creator: msg.sender,
+            startTime: _startTime,
+            endTime: _endTime,
+            options: _options,
+            isOpen: true
+        });
+
+        //write it to blockchain?
+
+        emit PollCreated(pollCount, msg.sender, _startTime, _endTime, _options);
+    }
 
     function getPollCount(){} //condition: only after poll ends
 
